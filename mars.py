edbcc2a00867fff1a550635942d14a8c2f75ee0f
empmaynmtusrgdyt <leammax@yandex.ru>
2025-03-11 22:28:11 +0300

    Third commit

--- mars.py
+++ mars.py
@@ -20,19 +20,29 @@ def prom():
     return '</br>'.join(lst)
 
 
-@app.route('/image_mars')
+@app.route('/promotion_image')
 def img():
+    lst = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+           'Мы сделаем обитаемыми безжизненные пока планеты.',
+           'И начнем с Марса!', 'Присоединяйся!']
+
+    colors = ['color-1', 'color-2', 'color-3', 'color-4', 'color-5']
+    colored_lines = [f'<h3 class="{colors[i % len(colors)]}">{line}</h3>' for i, line in
+                     enumerate(lst)]
     return f"""<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
                     <title>Привет, Марс!</title>
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+                  </head>
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="{url_for('static', filename='img/mars.jpg')
     }" alt="здесь должна была быть картинка марса">
-                    <h3>Вот она какая, красная планета<h3>
+                    {''.join(colored_lines)}
                   </body>
                 </html>"""
 
