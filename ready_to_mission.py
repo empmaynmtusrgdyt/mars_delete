a594697e7c572d5fae30777ff92d08c299486daf
empmaynmtusrgdyt <leammax@yandex.ru>
2025-03-17 22:34:57 +0300

    Second commit

--- ready_to_mission.py
+++ ready_to_mission.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('base.html', title='Заготовка')
 
+professions = [
+    "Пилот",
+    "Инженер",
+    "Врач",
+    "Геолог",
+    "Биолог",
+    "Программист",
+    "Строитель",
+    "Специалист по жизнеобеспечению",
+]
+
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    return render_template('base.html',
+                           title='Список профессий',
+                           list_type=list_type,
+                           professions=professions)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
